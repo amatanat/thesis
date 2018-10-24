ac9a7806c61b2d7b7cee78c6c72146a2207dd50e
amatanat <ahmadova.matanat01@gmail.com>
2018-10-24 22:24:56 +0200

    Matcher update. Delete fingerprint generation app's files. Include type in fingerprint matching.

--- matcher/matcher.py
+++ matcher/matcher.py
@@ -6,11 +6,12 @@ from sqlite3 import Error
 import re
 from collections import Counter 
 import json
+import subprocess
 
 def connect_to_db (db_file):
 	""" create a database connection to the SQLite database
         specified by the db_file
-    	return: Connection object or None"""
+    	return: Connection object or None """
     	try:
         	conn = sqlite3.connect(db_file)
 		print "connected to db.."
@@ -20,9 +21,10 @@ def connect_to_db (db_file):
 	return None
 
 def get_app_ids (db):
+	""" Return all applications' ID from a db """
 	ids = list()
 	cursor = db.cursor()
-	sql_select = "SELECT ID FROM application"
+	sql_select = "SELECT ID FROM applications"
 	cursor.execute(sql_select)
 	
 	rows = cursor.fetchall()
@@ -32,19 +34,22 @@ def get_app_ids (db):
 	return ids
 
 def get_app_name (db, app_id):
+	""" Return application name with the given app_id """
 	cursor = db.cursor()
-	sql_select = "SELECT name FROM application WHERE ID=?"
+	sql_select = "SELECT name FROM applications WHERE ID=?"
 	cursor.execute(sql_select, (app_id,))
 	rows = cursor.fetchall()
     	for row in rows:
 		for item in row:
 			return item
 
-def get_profiles_size (db,app_id):
+def get_fingerprints_size (db, app_id, file_type):
+	""" Get fingerprints' size list from a db with the 
+	given app_id and file_type """
 	size_list = list()
 	cursor = db.cursor()
-	sql_select = "SELECT file_size FROM profile WHERE application_id=?"
-	cursor.execute(sql_select, (app_id,))
+	sql_select = "SELECT file_size FROM fingerprints WHERE application_id=? AND file_type=?"
+	cursor.execute(sql_select, (app_id,file_type,))
 	rows = cursor.fetchall()
     	for row in rows:
 		for item in row:
@@ -54,34 +59,89 @@ def get_profiles_size (db,app_id):
 def compare (s, t):
     return Counter(s) == Counter(t)	
 
-def match_data (db, files_size):
+def extract_fingerprints_size (fingerprint_type, fingerprints):
+	""" Extract fingerprints' size with the given type
+	from a given list """
+	fingerprints_size = list()
+	for i in fingerprints:
+		if (i[2] == fingerprint_type):
+			fingerprints_size.append(i[1])
+	return fingerprints_size
+
+def match_data (db, fingerprint_type, fingerprints, file_type):
+	""" Return app_id if fingerprint match is found in a db"""
+	fingerprints_size = extract_fingerprints_size(fingerprint_type, fingerprints)
 	app_ids = get_app_ids(db)
 	for app_id in app_ids:
-		l = get_profiles_size(db,app_id)
-		if compare(l,files_size):
-			return get_app_name(db,app_id)
+		db_app_fingerprints_size = get_fingerprints_size(db, app_id, file_type)
+		if compare(db_app_fingerprints_size,fingerprints_size):
+			return app_id
+
+def get_app (db, oat_app_id, lib_app_id, root_app_id):
+	""" Return app_name if all app ids are equal,
+	i.e all fingerprints belong to the same app in a db """
+	if (oat_app_id == lib_app_id and oat_app_id == root_app_id):
+		return get_app_name(db, root_app_id)
+
+def find_matches (fingerprints):
+	""" Return app name if match is found in a db for given fingerprints """
+	root_app_id = match_data(db, 'root', fingerprints, 'root') 	
+	
+	# if both 'lib' and 'oat' contain 3 files
+	if (any(e[2] == 'unknown' for e in fingerprints)):
+	
+		oat_app_id_1 = match_data(db, 'oat', fingerprints, 'oat')
+		lib_app_id_1 = match_data(db, 'unknown', fingerprints, 'lib')
+
+		app_name_1 = get_app(db, oat_app_id_1, lib_app_id_1, root_app_id)
+					
+		oat_app_id_2 = match_data(db, 'unknown', fingerprints, 'oat')
+		lib_app_id_2 = match_data(db, 'oat', fingerprints, 'lib')
+					
+		app_name_2 = get_app(db, oat_app_id_2, lib_app_id_2, root_app_id) 
+
+		if app_name_1 is not None:
+			return app_name_1
+		elif app_name_2 is not None:
+			return app_name_2
+
+	else: 
+		oat_app_id = match_data(db, 'oat', fingerprints, 'oat')
+
+		# if any 'lib' file is available
+		if (any(e[2] == 'lib' for e in fingerprints)):	
+			lib_app_id = match_data(db, 'lib', fingerprints, 'lib')
+
+			app_name = get_app(db, oat_app_id, lib_app_id, root_app_id)
+			if app_name is not None:
+				return app_name
+
+		elif (oat_app_id == root_app_id and root_app_id is not None):
+			return get_app_name(db, root_app_id)
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
-		print "Usage: python matcher.py <application_profiles_db_name> <xml_extracted_files_size_filename>"
+		print "Usage: python matcher.py <fingerprints_db_name> <xml_dump_name>"
 		exit()
 
 	db_file_name = sys.argv[1] 
-	xml_extracted_files_size = sys.argv[2]
+	xml_dump = sys.argv[2]
+ 
+	subprocess.call(["python", "xml_fingerprints_extractor.py", xml_dump, "xml_extracted_fingerprints"])
+	xml_extracted_fingerprints = "xml_extracted_fingerprints.txt"
 
 	db = connect_to_db(db_file_name)
 	if db is not None:
-		with open(xml_extracted_files_size, 'r') as f:
+		with open(xml_extracted_fingerprints, 'r') as f:
     			data = json.load(f)
 			app_name_dict = {}
+
 			for key, value in data.items():
-				files_size = list()
-				for i in value:				
-					files_size.append(i[1])
-				app_name = match_data(db, files_size)
+				app_name = find_matches(value)
 				if app_name is not None:
 					app_name_dict[key] = app_name
-
+				
 			for k,v in app_name_dict.items():
 				print k,v
 		
---
+++ matcher/xml_fingerprints_extractor.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+
+import xml.etree.ElementTree as ET
+import sys
+import collections
+import json
+
+fingerprints = {}
+check_three = False
+
+def get_app_folder_inode ():
+	return root.find("./fileobject/[filename='app']").find('inode').text
+
+def create_dict (inode):
+	global check_three
+	check_three = False
+
+	fingerprints[inode] = list()
+	find_fingerprints(inode, inode, 'root')
+
+def find_app_subfolders (inode):
+	for fileobject in e.findall('fileobject'):
+		parent_inode = fileobject.find('parent_object').find('inode').text
+		name_type = fileobject.find('name_type').text 
+		i_node = fileobject.find('inode').text
+		if (parent_inode is not None and int(parent_inode) == int(inode) and 
+			str(name_type) == str("d/d")):
+
+			create_dict(int(i_node))
+
+def find_children_count (inode):
+	""" Return children files' count in a given folder """
+	count = 0
+	for fileobject in e.findall('fileobject'):
+		parent_inode = fileobject.find('parent_object').find('inode').text
+		name_type = fileobject.find('name_type').text 
+		if (parent_inode is not None and int(parent_inode) == int(inode) and 
+			str(name_type) == str("r/r")):
+			count += 1
+	return count
+
+def find_type (count):
+	if count != 3:	
+		return 'lib'
+	else:	
+		global check_three
+		if not check_three:
+			check_three = True
+			return 'oat'
+		else:
+			return 'unknown'
+		
+
+def find_fingerprints (inode, root_folder_inode, file_type):
+	for fileobject in e.findall('fileobject'):
+		parent_inode = fileobject.find('parent_object').find('inode').text
+		name_type = fileobject.find('name_type').text 
+		filesize = fileobject.find('filesize').text 
+		i_node = fileobject.find('inode').text
+
+		if (parent_inode is not None and int(parent_inode) == int(inode) and 
+			str(name_type) == str("d/d") and filesize is not None):
+			
+			file_type = find_type(find_children_count(i_node))
+			find_fingerprints(i_node, root_folder_inode, file_type)
+
+		elif (parent_inode is not None and int(parent_inode) == int(inode) and
+			str(name_type) == "r/r" and filesize is not None):
+			
+			if (int(parent_inode) == root_folder_inode):
+				file_type = 'root'
+
+			fingerprints[root_folder_inode].append([int(i_node), int(filesize), file_type])
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print "Usage: python xml_fingerprints_extractor.py <xml_dump_name> <output_filename>"
+		exit()
+
+	xml_dump = sys.argv[1] 
+	output = sys.argv[2] + ".txt"
+	root = ET.parse(xml_dump).getroot()
+	xmlstr = ET.tostring(root, encoding='utf8', method='xml')
+	e = ET.fromstring(xmlstr)
+
+	inode = get_app_folder_inode()
+	find_app_subfolders(inode)
+	with open(output, 'w+') as f:
+     		f.write(json.dumps(fingerprints))
+
+
