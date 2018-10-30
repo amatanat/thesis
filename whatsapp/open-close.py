4a8f8a0149eafcff4775f40a13d4f494d14fb9fb
amatanat <ahmadova.matanat01@gmail.com>
2018-10-30 21:13:23 +0100

    Update: Add logging. Remove an app from the recent task list. Add an app version and device time.

--- whatsapp/open-close.py
+++ whatsapp/open-close.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from com.dtmilano.android.viewclient import ViewClient
+import logging
+import logging.config
 
 device, serialno = ViewClient.connectToDeviceOrExit()
 vc = ViewClient(device,serialno)
 
-print "launching app..."
+app_version = device.shell("dumpsys package com.whatsapp | grep versionName").strip().split("=")[1]
+device_time = device.shell("date '+%F %X'").strip()
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('WhatsApp')
+d={'datetime': device_time, 'version': app_version, 'action': 'open-close'}
 
 # set a variable with the package's internal name
 package = 'com.whatsapp'
@@ -19,10 +26,19 @@ runComponent = package + '/' + activity
 
 # run the component
 device.startActivity(component=runComponent)
+logger.info('open application',extra=d)
 				
 # wait 
 ViewClient.sleep(3)
 
 # close the app
 device.shell('am force-stop com.whatsapp')
+logger.info('stop the application',extra=d)
+
+# remove the app from the recent task list
+device.shell('input keyevent KEYCODE_APP_SWITCH')
+device.shell('input keyevent DEL')
+logger.info('remove an app from the recent list',extra=d)
+device.shell('input keyevent KEYCODE_HOME')
+logger.info('return HOME',extra=d)
 
