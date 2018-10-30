2c363652f3344fb1615c54bcda951c351e16e5a4
amatanat <ahmadova.matanat01@gmail.com>
2018-10-30 21:14:45 +0100

    Update: Add logging. Add an app version and device time. Remove an app from the recent task list.

--- whatsapp/push-notification.py
+++ whatsapp/push-notification.py
@@ -2,31 +2,48 @@
 # -*- coding: utf-8 -*-
 
 from com.dtmilano.android.viewclient import ViewClient
+import logging
+import logging.config
 
 device, serialno = ViewClient.connectToDeviceOrExit()
 vc = ViewClient(device,serialno)
 
-# open Navigation bar
-device.dragDip((168.0, 17.0), (184.0, 476.0), 1000, 20, 0)
-dump = vc.dump()
-count = 0
-for v in dump:
-	if v.package() == "com.whatsapp":
-		# A user received only one text message from a Contact
-		if v['resource-id'] == "android:id/big_text": 
-			print "A user received a new text message."
-		# A user received multiple messages from a Contact
-        	elif v['resource-id'].startswith("android:id/inbox_text"):
-			count += 1
-		# A user received a photo from a Contact
-		elif "Photo" in v.text():
-			print "A user received a new photo"
-		# Message from multiple contacts
-		elif v['resource-id'] == "android:id/header_text":
-			if "messages" and "chats" in v.text():
-				print "A user received new messages from multiple contacts."
+app_version = device.shell("dumpsys package com.whatsapp | grep versionName").strip().split("=")[1]
+device_time = device.shell("date '+%F %X'").strip()
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('WhatsApp')
+d={'datetime': device_time, 'version': app_version, 'action': 'push-notification'}
+
+try:
+	# open Navigation bar
+	device.dragDip((168.0, 17.0), (184.0, 476.0), 1000, 20, 0)
+	logger.info('open Navigation Bar',extra=d)
+
+	dump = vc.dump()
+	count = 0
+	for v in dump:
+		if v.package() == "com.whatsapp":
+			# A user received only one text message from a Contact
+			if v['resource-id'] == "android:id/big_text": 
+				logger.info('A user received a new text message',extra=d)
+			# A user received multiple messages from a Contact
+			elif v['resource-id'].startswith("android:id/inbox_text"):
+				count += 1
+			# A user received a photo from a Contact
+			elif "Photo" in v.text():
+				logger.info('A user received a new photo',extra=d)
+			# Message from multiple contacts
+			elif v['resource-id'] == "android:id/header_text":
+				if "messages" and "chats" in v.text():
+					logger.info('A user received new messages from multiple contacts',extra=d)
 		
-if count > 0:
-	print "A user received", count, "messages."
-	
+	if count > 0:
+		logger.info('A user received %d messages',count,extra=d)
+
+except Exception as e:
+	logger.exception('Exception',extra=d)
+
+device.shell('input keyevent KEYCODE_HOME')
+logger.info('return HOME',extra=d)
 
--- whatsapp/receive-messages.py
+++ whatsapp/receive-messages.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
-import sys
-import os
-
 from com.dtmilano.android.viewclient import ViewClient
+import logging
+import logging.config
+import time
 
 def get_previous_view_unique_id (unique_id):
 	unique_id = unique_id.split("/")
@@ -16,14 +15,23 @@ def find_message_type (dump, view_id):
 		if v.uniqueId() == view_id:
 			# for the photo message the view id before the emoji button is the 'forward' image's id.
 			if v['resource-id'] == "com.whatsapp:id/forward":
-				print "last message is a photo."
+				logger.info('last received message is a photo',extra=d)
 			else:
-				print "last message is a text."
+				logger.info('last received message is a text',extra=d)
+
+def get_device_time(ts):
+	return time.strftime("%FT%TZ",time.gmtime(ts))
 
 device, serialno = ViewClient.connectToDeviceOrExit()
 vc = ViewClient(device,serialno)
 
-print "launching app..."
+app_version = device.shell("dumpsys package com.whatsapp | grep versionName").strip().split("=")[1]
+device_time = device.shell("date '+%F %X'").strip()
+#get_device_time(device.shell("echo $EPOCHREALTIME"))
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('WhatsApp')
+d={'datetime': device_time, 'version': app_version, 'action': 'receive-message'}
 
 # set a variable with the package's internal name
 package = 'com.whatsapp'
@@ -36,11 +44,13 @@ runComponent = package + '/' + activity
 
 # run the component
 device.startActivity(component=runComponent)
+logger.info('open application',extra=d)
 
 dump=vc.dump()
 for view in dump:
 	if view['resource-id'] == "com.whatsapp:id/conversations_row_message_count":
 		view.touch()
+		logger.info('click new message received conversation',extra=d)
 		view_dump = vc.dump()
 		for v in view_dump:
 			if v['resource-id'] == "com.whatsapp:id/emoji_picker_btn":
@@ -51,10 +61,20 @@ for view in dump:
 				# Navigate up 
 				com_whatsapp___id_back = vc.findViewByIdOrRaise("com.whatsapp:id/back")
 				com_whatsapp___id_back.touch()
+				logger.info('navigate to main screen',extra=d)
 				
 				# update the dump
 				dump = vc.dump()
 				break
+
 # close the app
 device.shell('am force-stop com.whatsapp')
+logger.info('stop the application',extra=d)
+
+# remove the app from the recent task list
+device.shell('input keyevent KEYCODE_APP_SWITCH')
+device.shell('input keyevent DEL')
+logger.info('remove an app from the recent list',extra=d)
+device.shell('input keyevent KEYCODE_HOME')
+logger.info('return HOME',extra=d)
 
