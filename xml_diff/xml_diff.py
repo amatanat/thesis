27f5543327b8d0adb2035e6ce02e8427a3f1e5fe
amatanat <ahmadova.matanat01@gmail.com>
2018-11-04 21:07:50 +0100

    Output result to a JSON file.

--- xml_diff/xml_diff.py
+++ xml_diff/xml_diff.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import json
 import xml.etree.ElementTree as ET
 from file_object import file_object
 from datetime import datetime
@@ -11,7 +12,8 @@ moved_files = set()
 changed_metadata = set()
 deleted_files = set()
 
-def get_file_object_dict (root, file_object_dict):
+def get_file_objects_dict (root):
+	file_objects_dict = {}
 	for fo in root.findall('fileobject'):
 		parent_inode = fo.find('parent_object').find('i_node')
 		if parent_inode is not None:
@@ -52,9 +54,9 @@ def get_file_object_dict (root, file_object_dict):
 		
 		fileobject = file_object(filename, name_md5, name_B64, name_type, filesize, inode, parent_inode, mode, nlink, namesize, uid, gid,genId, mtime, atime, ctime, crtime, xnonce, xmaster, xNameCipher)
 		
-		file_object_dict[inode] = fileobject
+		file_objects_dict[inode] = fileobject
 
-	return file_object_dict
+	return file_objects_dict
 
 def compare_dictionaries (old, new):
 	for key, n_fo in new.items():
@@ -84,48 +86,88 @@ def get_datetime (str_datetime):
 	datetime_object = datetime.strptime(str_datetime, '%Y-%m-%dT%H:%M:%SZ')
 	return datetime_object.isoformat(' ')
 
-def print_set (data):
-	"""Print set without comma and brackets"""
-	for i in data:
-		print ' '.join([str(k) for k in i]) 
-
-def print_file_objects (title, file_objects):
-	print "\n" + title +  "\n"
+def get_file_objects_data (fileobjects):
+	"""Take a set of file_objects as input and
+	return a sorted set containing: ('mtime', 'inode') for each file_object""" 
 	result = set()
-	for fo in file_objects:
+	for fo in fileobjects:
 		result.add((get_datetime(fo["mtime"]), fo["inode"]))
-	print_set(sorted(result))
+	return sorted(result)
 
-def print_file_objects2 (title, file_objects):
-	print "\n" + title +  "\n"
+def get_file_objects2_data (fileobjects):
+	"""Take a set containing tuples of file_objects as input
+	 for each tuple: compare file_objects data
+	 return a sorted set containing different data"""
 	result = set()
-	for (o_fo, n_fo) in file_objects:
+	for (o_fo, n_fo) in fileobjects:
 		if o_fo["filename"] != n_fo["filename"]:				
 			result.add((get_datetime(n_fo["mtime"]), n_fo["inode"], "filename changed"))
 
 		if o_fo["parent_inode"] != n_fo["parent_inode"]:
-			result.add((get_datetime(n_fo["mtime"]), n_fo["inode"], "file moved, parent inode changed", o_fo["parent_inode"], "=>", n_fo["parent_inode"]))
+			result.add((get_datetime(n_fo["mtime"]), n_fo["inode"], "file moved, parent inode changed", o_fo["parent_inode"], 
+			n_fo["parent_inode"]))
 
 		if o_fo["mtime"] != n_fo["mtime"]:
-			result.add((get_datetime(n_fo["mtime"]), n_fo["inode"], "mtime changed", get_datetime(o_fo["mtime"]), "=>",get_datetime(n_fo["mtime"])))
+			result.add((get_datetime(n_fo["mtime"]), n_fo["inode"], "mtime changed", get_datetime(o_fo["mtime"])))
 
 		if o_fo["atime"] != n_fo["atime"]:
-			result.add((get_datetime(n_fo["atime"]), n_fo["inode"], "atime changed", get_datetime(o_fo["atime"]), "=>", get_datetime(n_fo["atime"])))
+			result.add((get_datetime(n_fo["atime"]), n_fo["inode"], "atime changed", get_datetime(o_fo["atime"])))
 
 		if o_fo["ctime"] != n_fo["ctime"]:
-			result.add((get_datetime(n_fo["ctime"]), n_fo["inode"], "ctime changed", get_datetime(o_fo["ctime"]), "=>", get_datetime(n_fo["ctime"])))
+			result.add((get_datetime(n_fo["ctime"]), n_fo["inode"], "ctime changed", get_datetime(o_fo["ctime"])))
 
 		if o_fo["crtime"] != n_fo["crtime"]:
-			result.add((get_datetime(n_fo["crtime"]), n_fo["inode"], "crtime changed", get_datetime(o_fo["crtime"]), "=>", get_datetime(n_fo["crtime"])))
+			result.add((get_datetime(n_fo["crtime"]), n_fo["inode"], "crtime changed", get_datetime(o_fo["crtime"])))
 
-	print_set (sorted(result))
+	return sorted(result)
 
-def final_report() :
-	print_file_objects("New files:", new_files)
-	print_file_objects("Deleted files:", deleted_files)
-	print_file_objects2("Renamed files:", renamed_files)
-	print_file_objects2("Moved files:", moved_files)
-	print_file_objects2("Changed metadata:", changed_metadata)	
+def append_to_output (key, value, output_data):
+	"""Append a new key and value to an output data"""
+	output_data[key] = []
+	for fo in value:
+		data = {
+			'datetime' : str(fo[0]),
+			'inode' : str(fo[1])
+			}
+		output_data[key].append(data)	
+
+def append_to_output2 (key, value, output_data):
+	output_data[key] = []
+	for fo in value:
+		if len(fo) == 3:
+			data = {
+				'datetime' : str(fo[0]),
+				'inode' : str(fo[1]),
+				'action': str(fo[2])
+				}
+		elif len(fo) == 4:
+			data = {
+				'datetime' : str(fo[0]),
+				'inode' : str(fo[1]),
+				'action': str(fo[2]),
+				'previous_datetime' : str(fo[3])
+				}
+		elif len(fo) == 5:
+			data = {
+				'datetime' : str(fo[0]),
+				'inode' : str(fo[1]),
+				'action': str(fo[2]),
+				'previous_parent_inode' : str(fo[3]),
+				'current_parent_inode' : str(fo[4])
+				}
+		output_data[key].append(data)	
+
+def output_json() :
+	
+	output_data = {}
+	append_to_output('New files', get_file_objects_data(new_files), output_data)
+	append_to_output('Deleted files', get_file_objects_data(deleted_files), output_data)
+	append_to_output2("Renamed files:", get_file_objects2_data(renamed_files), output_data)
+	append_to_output2("Moved files:", get_file_objects2_data(moved_files), output_data)
+	append_to_output2("Changed metadata:", get_file_objects2_data(changed_metadata), output_data)
+
+	with open('data.json', 'w+') as outfile:
+    		json.dump(output_data, outfile)
 	
 
 if __name__ == "__main__":
@@ -137,16 +179,14 @@ if __name__ == "__main__":
 
 	first_xml_dump = sys.argv[1] 
 	first_root = ET.parse(first_xml_dump).getroot()
-	old = {}
-	old = get_file_object_dict(first_root, old)
+	old = get_file_objects_dict(first_root)
 	
 	second_xml_dump = sys.argv[2] 
 	second_root = ET.parse(second_xml_dump).getroot()
-	new = {}
-	new = get_file_object_dict(second_root, new)
+	new = get_file_objects_dict(second_root)
 
 	compare_dictionaries(old, new)
 
-	final_report()
+	output_json()
 			
 
