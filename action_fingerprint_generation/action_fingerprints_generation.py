67a141153603312793bd43d067e218149d9b9580
amatanat <ahmadova.matanat01@gmail.com>
2018-11-27 16:44:26 +0100

    Replace inode with the tree structure.

--- action_fingerprint_generation/action_fingerprints_generation.py
+++ action_fingerprint_generation/action_fingerprints_generation.py
@@ -15,16 +15,23 @@ def connect_to_db (db_file):
         	print(e)
 	return None
 
-def insert_action_fingerprints (db, app_name, action_name, key, changed, inode): 
-	sql_insert = "INSERT INTO action_fingerprints (app_name, action_name, key, changed, inode) VALUES(?,?,?,?,?)"
+def insert_action_fingerprints (db, app_name, action_name, path, changed): 
+	sql_insert = "INSERT INTO action_fingerprints (app_name, action_name, path, changed) VALUES(?,?,?,?)"
    	cursor = db.cursor()
-    	cursor.execute(sql_insert, (app_name, action_name, key, changed, inode))
+    	cursor.execute(sql_insert, (app_name, action_name, path, changed))
    	return cursor.lastrowid
 
 def find_action_fingerprints (db, sql_query, app_name, action_name):
 	cursor = db.cursor()
 	cursor.execute(sql_query,  [action_name, app_name, action_name, app_name])
 	return cursor.fetchall()
+
+def get_all_action_fingerprints (db):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM action_fingerprints")
+    rows = cursor.fetchall()
+    for row in rows:
+	print(row)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 4:
@@ -35,11 +42,11 @@ if __name__ == '__main__':
 	app_name = sys.argv[2]
 	action_name = sys.argv[3]
 
-	sql_query =  """SELECT count(*), app_name, inode, changed, action_name, key 
+	sql_query =  """SELECT count(*), app_name, action_name, path, changed 
 		        FROM action_profiles
 		        WHERE action_profiles.action_name = ? 
 			AND action_profiles.app_name = ?
-			GROUP BY inode, changed
+			GROUP BY path, changed
 			HAVING count (*) = (
 				SELECT MAX(run)
 				FROM action_profiles AS profiles
@@ -52,7 +59,7 @@ if __name__ == '__main__':
 	if db is not None:
 		rows = find_action_fingerprints(db, sql_query, app_name, action_name)
 		for row in rows:
-			insert_action_fingerprints (db, app_name, action_name, row[5], row[3], row[2])
+			insert_action_fingerprints (db, app_name, action_name, row[3], row[4])
 		db.commit()
 		
 	else:
