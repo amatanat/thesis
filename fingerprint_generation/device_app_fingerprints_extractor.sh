0c786abf52a649a904edcd76461193d696f4215f
amatanat <ahmadova.matanat01@gmail.com>
2018-12-10 12:37:32 +0100

    adb shell: extract app version.

--- fingerprint_generation/device_app_fingerprints_extractor.sh
+++ fingerprint_generation/device_app_fingerprints_extractor.sh
@@ -2,6 +2,7 @@
 
 extract_app_fingerprints () {
 	echo "name-"$1 >> "device_app_fingerprints.txt"
+	echo $2 >> "device_app_fingerprints.txt"
 	R=$(adb shell "
 		su -c ' cd /data/app/
 			cd \$(ls -d * | grep \"$1\")
@@ -12,8 +13,10 @@ extract_app_fingerprints () {
 for app_name in $(adb shell "
  		su -c 'cmd package list packages -3 | cut -f 2 -d \":\" '"); do
 
-	app_name=${app_name%$'\r'}		
-	extract_app_fingerprints "$app_name" 
+	app_name=${app_name%$'\r'}
+	version_name=$(adb shell "
+		su -c 'dumpsys package \"$app_name\" | grep -i versionName'")
+	extract_app_fingerprints "$app_name" "$version_name"
 	
 done
 
