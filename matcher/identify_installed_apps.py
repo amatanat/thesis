551827e7626ce818da77266a601d8afafd93e521
amatanat <ahmadova.matanat01@gmail.com>
2019-01-29 13:22:51 +0100

    Return 'unknown' if app could not be identified. Return all found app matches.

--- matcher/identify_installed_apps.py
+++ matcher/identify_installed_apps.py
@@ -67,54 +67,63 @@ def extract_fingerprints_size (fingerprint_type, fingerprints):
 	return fingerprints_size
 
 def match_data (db, fingerprint_type, fingerprints, file_type):
-	""" Return app_id if fingerprint match is found in a db"""
+	""" Return app_id list if fingerprint match is found in a db"""
+	result = list()
 	fingerprints_size = extract_fingerprints_size(fingerprint_type, fingerprints)
 	app_ids = get_app_ids(db)
 	for app_id in app_ids:
 		db_app_fingerprints_size = get_fingerprints_size(db, app_id, file_type)
 		if compare(db_app_fingerprints_size,fingerprints_size):
-			return app_id
+			result.append(app_id)
+	return result
 
-def get_app (db, lib_app_id, root_app_id):
-	""" Return app_name if all app ids are equal,
-	i.e all fingerprints belong to the same app in a db """
-	if (lib_app_id == root_app_id):
-		return get_app_name(db, root_app_id)
+def get_app (root_id_list, lib_id_list):
+	"""Return app-name list if 'lib' and 'root' files belong
+	to the same app in a DB"""
+	result = list()
+	# find the same values in both lists
+	app_ids = list(set(lib_id_list).intersection(root_id_list))
+	# if any values match
+	if len(app_ids) > 0:
+		for app_id in app_ids:
+			# get app-name correspinding to the given id
+			result.append(get_app_name(db, app_id))
+	return result
 
 def find_matches (fingerprints):
 	""" Return an app name if match is found in a db for given fingerprints """
-
-	root_app_id = match_data(db, 'root', fingerprints, 'root') 	
+	result = list()
+	root_app_id_list = match_data(db, 'root', fingerprints, 'root')
 	
 	# if both 'lib' and 'oat' contain 2 or 3 files
 	if (any(e[2] == 'unknown' for e in fingerprints)):
 	
 		# match 'unknown' with 'lib' files
-		lib_app_id_one = match_data(db, 'unknown', fingerprints, 'lib')
-		app_name_one = get_app(db, lib_app_id_one, root_app_id)
+		lib_app_id_list_one = match_data(db, 'unknown', fingerprints, 'lib')
+		app_names = get_app(root_app_id_list, lib_app_id_list_one)
+		if len(app_names) > 0:
+			for data in app_names:
+				result.append(data)
 		
 		# match 'oat' with 'lib' files			
-		lib_app_id_two = match_data(db, 'oat', fingerprints, 'lib')
-		app_name_two = get_app(db, lib_app_id_two, root_app_id) 
-
-		if app_name_one is not None and app_name_two is not None:
-			return (app_name_one, app_name_two)	
-		if app_name_one is not None:
-			return app_name_one
-		if app_name_two is not None:
-			return app_name_two
+		lib_app_id_list_two = match_data(db, 'oat', fingerprints, 'lib')
+		app_names = get_app(root_app_id_list, lib_app_id_list_two)
+		if len(app_names) > 0:
+			for data in app_names:
+				result.append(data)
+		return result
 	 
 	# if any 'lib' file is available
 	if (any(e[2] == 'lib' for e in fingerprints)):	
-		lib_app_id = match_data(db, 'lib', fingerprints, 'lib')
-
-		app_name = get_app(db, lib_app_id, root_app_id)
-		if app_name is not None:
-			return app_name
+		lib_app_id_list = match_data(db, 'lib', fingerprints, 'lib')
+		result = get_app(root_app_id_list, lib_app_id_list)
+		return result
 
 	# no 'lib' file, only 'root' file is available
-	if (root_app_id is not None):
-		return get_app_name(db, root_app_id)
+	if (root_app_id_list is not None):
+		for app_id in root_app_id_list:
+			result.append(get_app_name(db, app_id))
+		return result
 
 def output_result(result):
 	with open(output, 'w+') as f:
@@ -141,8 +150,15 @@ if __name__ == '__main__':
 
 			for key, value in data.items():
 				app_name = find_matches(value)
-				if app_name is not None:
-					app_name_dict[key] = app_name
+				#if any match is found
+				if len(app_name) > 0:
+					for name in app_name:
+						if key in app_name_dict:
+							app_name_dict[key] = [app_name_dict[key],name]
+						else:
+							app_name_dict[key] = name
+				else:
+					app_name_dict[key] = "unknown"
 
 			# if any match is found
 			if (len(app_name_dict)) > 0 :
