e7512b5cf3647db21dcdffe4407556941983361d
amatanat <ahmadova.matanat01@gmail.com>
2019-01-31 21:16:34 +0100

    Save result to the specified output file.

--- fingerprint_generation/device_app_fingerprints_extractor.sh
+++ fingerprint_generation/device_app_fingerprints_extractor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 extract_app_fingerprints () {
-	echo "name-"$1 >> "device_app_fingerprints.txt"
-	echo $2 >> "device_app_fingerprints.txt"
+	echo "name-"$1 >> $3
+	echo $2 >> $3
 	R=$(adb shell "
 		su -c ' cd /data/app/
 			cd \$(ls -d * | grep \"$1\")
 			find * -type f | egrep -v '^oat' |xargs stat -c \"%s %n\" '") 
-	echo "$R" >> "device_app_fingerprints.txt"
+	echo "$R" >> $3
 }
 
 for app_name in $(adb shell "
@@ -16,7 +16,7 @@ for app_name in $(adb shell "
 	app_name=${app_name%$'\r'}
 	version_name=$(adb shell "
 		su -c 'dumpsys package \"$app_name\" | grep -i versionName'")
-	extract_app_fingerprints "$app_name" "$version_name"
+	extract_app_fingerprints "$app_name" "$version_name" "$1"
 	
 done
 
