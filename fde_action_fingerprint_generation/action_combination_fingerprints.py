07793ec09b3d7a175693aa30befddd504011f4e2
amatanat <ahmadova.matanat01@gmail.com>
2019-02-12 16:19:05 +0100

    Consider renamed columns. Update the query.

--- fde_action_fingerprint_generation/action_combination_fingerprints.py
+++ fde_action_fingerprint_generation/action_combination_fingerprints.py
@@ -16,47 +16,54 @@ def connect_to_db (db_file):
         	print(e)
 	return None
 
-def insert_action_combination_fingerprints (db, app_name, action_name, action_name_list, path, changed): 
-	sql_insert = """INSERT INTO action_combination_fingerprints (app_name, action_name, included_actions, path, changed)
+def insert_action_combination_fingerprints (db, app_name,  included_actions, excluded_actions, path, changed):
+	sql_insert = """INSERT INTO action_combination_fingerprints (app_name, included_actions, excluded_actions, path, changed)
 		      VALUES(?,?,?,?,?)"""
    	cursor = db.cursor()
-    	cursor.execute(sql_insert, (app_name, action_name, str(action_name_list), path, changed))
+ 	cursor.execute(sql_insert, (app_name, str(included_actions), str(excluded_actions), path, changed))
    	return cursor.lastrowid
 
-def find_action_combination_fingerprints(db, app_name, action_name, action_name_list):
-	placeholders= ', '.join(['?'] * len(action_name_list))
-	query = ''' SELECT app_name, action_name, path, changed
+def find_action_combination_fingerprints(db,  included_actions, excluded_actions):
+	in_placeholder = ', '.join(['?'] * len(included_actions))
+	not_in_placeholder = ', '.join(['?'] * len(excluded_actions))
+	query = ''' SELECT app_name, action_name, path, changed, count(*)
 		        FROM action_fingerprints
-		        WHERE action_fingerprints.action_name = {}
-			AND action_fingerprints.app_name = {}
-			AND action_fingerprints.path IN (
+		        WHERE action_fingerprints.action_name in ({})
+			AND action_fingerprints.path NOT IN (
 				SELECT path
 				FROM action_fingerprints AS fingerprints
 				WHERE fingerprints.app_name = action_fingerprints.app_name
 					AND fingerprints.changed = action_fingerprints.changed
 					AND fingerprints.action_name IN ({})
 				)
-			ORDER BY id ASC;'''.format("'"+action_name+"'", "'"+app_name+"'", placeholders)
+			GROUP BY path, changed
+			HAVING count(*) = {}
+			ORDER BY id ASC;'''.format(in_placeholder, not_in_placeholder, len(included_actions))
 
+	actions_list = list()
+	for action in included_actions:
+		actions_list.append(action)
+	for action in excluded_actions:
+		actions_list.append(action)
 	cursor = db.cursor()
-	cursor.execute(query, tuple(action_name_list))
+	cursor.execute(query, tuple(actions_list))
 	return cursor.fetchall()
 
 if __name__ == '__main__':
-	if len(sys.argv) < 5:
-		print "Usage: python action_combination_fingerprints.py <db_filename> <app_name> <action_name> <action_name_list>"
+	if len(sys.argv) < 4:
+		print "Usage: python action_combination_fingerprints.py <db_filename> <included_actions> <excluded_actions> <app_name>"
 		exit()
 
 	db_filename = sys.argv[1] 
-	app_name = sys.argv[2]
-	action_name = sys.argv[3]
-	action_name_list = ast.literal_eval(sys.argv[4])
+	included_actions = ast.literal_eval(sys.argv[2])
+	excluded_actions = ast.literal_eval(sys.argv[3])
+	app_name = sys.argv[4]
 
 	db = connect_to_db(db_filename)
 	if db is not None:
-		rows = find_action_combination_fingerprints(db, app_name, action_name, action_name_list)
+		rows = find_action_combination_fingerprints(db, included_actions, excluded_actions)
 		for row in rows:
-			insert_action_combination_fingerprints (db, app_name, action_name, action_name_list, row[2], row[3])
+			insert_action_combination_fingerprints (db, app_name, included_actions, excluded_actions, row[2], row[3])
 		db.commit()
 		
 	else:
