c4fdb4ccb8f9d111483f634736167a77f944a44f
amatanat <ahmadova.matanat01@gmail.com>
2019-02-19 15:04:58 +0100

    Removed threshold.

--- wa_user_action_fingerprinting_evaluation/validation-test/run_validation_test.sh
+++ wa_user_action_fingerprinting_evaluation/validation-test/run_validation_test.sh
@@ -6,14 +6,13 @@ source validation_config.conf
 validation_script_name=$VALIDATION_SCRIPT_NAME
 identified_actions_xml_path=$IDENTIFIED_ACTIONS_XML_PATH
 log_file_path=$LOG_FILE_PATH
-threshold=$THRESHOLD
 action_name_1=$FIRST_ACTION_NAME
 action_name_2=$SECOND_aCTION_NAME
 
 counter=0
 while [ $counter -le 9 ]
 do
-	./$validation_script_name "${identified_actions_xml_path}/${counter}/identified_user_actions.xml" "${log_file_path}/${counter}/whatsapp-actions.log" "$threshold" "$action_name_1" "$action_name_2" "$counter"
+	./$validation_script_name "${identified_actions_xml_path}/${counter}/identified_user_actions.xml" "${log_file_path}/${counter}/whatsapp-actions.log" "$action_name_1" "$action_name_2" "$counter"
 	((counter++))
 done
 echo "All done"
